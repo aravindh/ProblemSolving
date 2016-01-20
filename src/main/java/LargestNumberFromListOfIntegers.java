90d07c04947293d6934eeef591d6aa0611596efb
aravindh.ravindran <aravindh.ravindran@redbus.in>
2016-01-20 10:58:20 +0530

    -Adding SomOfTripletFromThreeLinkedListEqualToGivenNumber question

--- src/main/java/LargestNumberFromListOfIntegers.java
+++ src/main/java/LargestNumberFromListOfIntegers.java
@@ -16,34 +16,8 @@ public class LargestNumberFromListOfIntegers {
         return largestnumber;
     }
     public static void main(String[] args) {
-/*
+        ///TODO: add solution
         List<Integer> integers = Arrays.asList(3, 30, 34, 5, 9);
         System.out.println("Largest Number: "+getLargestNumber(integers));
-*/
-        List<List<Integer>> A = Arrays.asList(Arrays.asList(1, 2, 3, 4), Arrays.asList(5, 6, 7, 8), Arrays.asList(9, 10, 11, 12));
-        ArrayList<ArrayList<Integer>> B = performOps(A);
-        for (int i = 0; i < B.size(); i++) {
-            for (int j = 0; j < B.get(i).size(); j++) {
-                System.out.print(B.get(i).get(j) + " ");
-            }
-        }
-
-
-    }
-
-    static ArrayList<ArrayList<Integer>> performOps(List<List<Integer>> A) {
-        ArrayList<ArrayList<Integer>> B = new ArrayList<ArrayList<Integer>>();
-        for (int i = 0; i < A.size(); i++) {
-            B.add(new ArrayList<Integer>());
-
-            for (int j = 0; j < A.get(i).size(); j++) {
-                B.get(i).add(0);
-            }
-
-            for (int j = 0; j < A.get(i).size(); j++) {
-                B.get(i).set(A.get(i).size() - 1 - j, A.get(i).get(j));
-            }
-        }
-        return B;
     }
 }
---
+++ src/main/java/SomOfTripletFromThreeLinkedListEqualToGivenNumber.java
@@ -0,0 +1,12 @@
+/**
+ * Given three linked lists, say a, b and c, find one node from each list such that the sum of the values of the nodes is equal to a given number.
+ For example, if the three linked lists are 12->6->29, 23->5->8 and 90->20->59, and the given number is 101, the output should be tripel “6 5 90″.
+
+ In the following solutions, size of all three linked lists is assumed same for simplicity of analysis. solutions work for linked lists of different sizes also.
+ * Created by aravindhravindran on 19/1/16.
+ */
+public class SomOfTripletFromThreeLinkedListEqualToGivenNumber {
+    public static void main(String[] args) {
+
+    }
+}
