a4e64a948bb1b0e86fbd28868873efde7a58a301
aravindhravindran <aravindh.ravindran@gmail.com>
2016-08-05 15:37:33 +0530

    -Adding solution for MaxNonNegativeSubArray problem

--- src/main/java/MaxNonNegativeSubArray.java
+++ src/main/java/MaxNonNegativeSubArray.java
@@ -7,82 +7,93 @@ import java.util.List;
  */
 public class MaxNonNegativeSubArray {
     public static void main(String[] args) {
-        //ArrayList<Integer> integers = new ArrayList<Integer>(Arrays.asList(1, 2, 3,-7, 20, -10)); //[20]
-        //ArrayList<Integer> integers = new ArrayList<Integer>(Arrays.asList(0, 0, -1, 0 )); //return [0,0]
-        //ArrayList<Integer> integers = new ArrayList<Integer>(Arrays.asList(-1, -1, -1, -1, -1)); //[]
-        //ArrayList<Integer> integers = new ArrayList<Integer>(Arrays.asList(1,1,1,1,1)); //[1,1,1,1,1]
-        //ArrayList<Integer> integers = new ArrayList<Integer>(Arrays.asList(1967513926, 1540383426, -1303455736, -521595368)); //[1967513926 1540383426 ]
-        //ArrayList<Integer> integers = new ArrayList<Integer>(Arrays.asList(0,0)); //[]
-        ArrayList<Integer> integers = new ArrayList<Integer>(Arrays.asList(-52263)); //[]
-        System.out.println(""+maxset(integers));
+        System.out.println("input 1, 2, 3,-7, 20, -10 expected [20] actual "+maxset(new ArrayList<Integer>(Arrays.asList(1, 2, 3,-7, 20, -10)))); //[20]
+        System.out.println("input 0, 0, -1, 0  expected [0,0] actual "+maxset(new ArrayList<Integer>(Arrays.asList(0, 0, -1, 0 )))); //return [0,0]
+        System.out.println("input -1, -1, -1, -1, -1 expected [] actual "+maxset(new ArrayList<Integer>(Arrays.asList(-1, -1, -1, -1, -1)))); //[]
+        System.out.println("input 1,1,1,1,1 expected [1,1,1,1,1] actual "+maxset(new ArrayList<Integer>(Arrays.asList(1,1,1,1,1)))); //[1,1,1,1,1]
+        System.out.println("input 1967513926, 1540383426, -1303455736, -521595368 expected [1967513926 1540383426 ] actual "+maxset(new ArrayList<Integer>(Arrays.asList(1967513926, 1540383426, -1303455736, -521595368)))); //[1967513926 1540383426 ]
+        System.out.println("input 0,0 expected [0,0] actual "+maxset(new ArrayList<Integer>(Arrays.asList(0,0)))); //[0,0]
+        System.out.println("input -52263 expected [] actual "+maxset(new ArrayList<Integer>(Arrays.asList(-52263)))); //[]
 
     }
 
-    public static ArrayList<Integer> maxset(ArrayList<Integer> a) {
-        int prevSumStartIdx = -1;
-        int prevSumEndIdx = -1;
-        long prevSum = 0;
-
-        int currSumStartIdx ;
-        int currSumEndIdx = 0;
-        long currSum = 0;
-
-        currSumStartIdx = -1;
-        for(int i = 0; i <a.size(); i++){
-            int num = a.get(i);
-
-            if( num < 0){
-                if(prevSum <= currSum){
-                    prevSum = currSum;
-                    prevSumEndIdx = currSumEndIdx;
-                    prevSumStartIdx = currSumStartIdx;
+    private static Boolean isCurrentGreatest(Integer currStartIdx, Integer currEndIdx, Double currSum,
+                                      Integer prevStartIdx, Integer prevEndIdx, Double prevSum){
+        if(prevStartIdx == null || prevEndIdx == null){
+            return true;
+        }
+        if(currSum > prevSum){
+            return true;
+        }else {
+            if(currSum == prevSum){
+                Integer prevLength = prevEndIdx - prevStartIdx;
+                Integer currLength = currEndIdx - currStartIdx;
+                if(currLength > prevLength){
+                    return true;
+                }else if(currLength < prevLength){
+                    return false;
+                }else{
+                    if(prevStartIdx < currStartIdx){
+                        return false;
+                    }else{
+                        return true;
+                    }
                 }
-                currSum = 0;
-                currSumStartIdx = i;
             }else{
-                currSumEndIdx = i;
-                currSum = num + currSum;
+                return false;
             }
         }
 
-        currSumStartIdx++;
-        currSumEndIdx++;
+    }
+    public static ArrayList<Integer> maxset(ArrayList<Integer> a) {
+        Integer prev = null;
 
+        Integer currStartIdx = null;
+        Integer currEndIdx = null;
+        Double currSum  = 0d;
 
-        ArrayList<Integer> prevSubList = null;
-        if(prevSumStartIdx> prevSumEndIdx)
-            prevSubList = new ArrayList<Integer>();
-        else{
-            prevSumStartIdx++;prevSumEndIdx++;
-            prevSubList = new ArrayList<Integer>(a.subList(prevSumStartIdx, prevSumEndIdx));
-        }
+        Integer prevStartIdx = null;
+        Integer prevEndIdx = null;
+        Double prevSum  = 0d;
 
-        ArrayList<Integer> integers = null;
-        if(currSumStartIdx > currSumEndIdx)
-            integers = new ArrayList<Integer>();
-        else{
-            integers = new ArrayList<Integer>(a.subList(currSumStartIdx, currSumEndIdx));
-        }
-        System.out.println(currSumStartIdx);
-        System.out.println(prevSumStartIdx);
-        if(prevSum < currSum){
-            return integers;
-        }else {
-            if( prevSum > currSum){
-                return prevSubList;
+        for(Integer idx = 0 ; idx < a.size(); idx++){
+            Integer curr = a.get(idx);
+
+            if(curr >= 0){
+                if(currStartIdx == null){
+                    currStartIdx = idx;
+                }
+                currEndIdx = idx;
+                currSum = currSum +curr;
             }else{
-                if(prevSubList.size() > integers.size()){
-                    return prevSubList;
-                }else if(prevSubList.size() < integers.size()){
-                    return integers;
-                }else{
-                    if(prevSumStartIdx < currSumStartIdx){
-                        return prevSubList;
-                    }else{
-                        return integers;
+                if(prev != null ) {
+                    if (prev >= 0 && curr < 0) {
+                        if (isCurrentGreatest(currStartIdx, currEndIdx, currSum,
+                                prevStartIdx, prevEndIdx, prevSum)) {
+                            prevStartIdx = currStartIdx;
+                            prevEndIdx = currEndIdx;
+                            prevSum = currSum;
+                        }
+                        currStartIdx = null;
+                        currEndIdx = null;
+                        currSum = 0d;
                     }
                 }
             }
+            prev = curr;
+        }
+
+        if((currStartIdx == null || currEndIdx == null) && (prevStartIdx == null || prevEndIdx == null)){
+            return new ArrayList<Integer>(0);
+        }else{
+            if(isCurrentGreatest(currStartIdx, currEndIdx, currSum,
+                    prevStartIdx, prevEndIdx, prevSum)){
+                return new ArrayList<Integer>(a.subList(currStartIdx, ++currEndIdx));
+
+            }else{
+                return new ArrayList<Integer>(a.subList(prevStartIdx, ++prevEndIdx));
+            }
+
         }
     }
 }
